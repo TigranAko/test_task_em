079be9818c24976f4e7403297be2f1e684ac881b
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-02-01 23:16:15 +0300

    feat(repo): add methods for get id by name in RoleRepository and BusinessElementRepository

--- app/repositories/business_elements.py
+++ app/repositories/business_elements.py
@@ -1,6 +1,13 @@
 from utils.base_repository import SQLAlchemyRepository
 from models.business_elements import BusinessElement
+from sqlalchemy import select
 
 
 class BusinessElementRepository(SQLAlchemyRepository):
     model = BusinessElement
+
+    def get_id(self, name: str) -> int:
+        stmt = select(self.model.id).where(self.model.name == name)
+        result = self.session.execute(stmt)
+        answer = result.scalar_one_or_none()
+        return answer
--- app/repositories/roles.py
+++ app/repositories/roles.py
@@ -1,6 +1,13 @@
 from utils.base_repository import SQLAlchemyRepository
 from models.roles import Role
+from sqlalchemy import select
 
 
 class RoleRepository(SQLAlchemyRepository):
     model = Role
+
+    def get_id(self, name: str) -> int:
+        stmt = select(self.model.id).where(self.model.name == name)
+        result = self.session.execute(stmt)
+        answer = result.scalar_one_or_none()
+        return answer
